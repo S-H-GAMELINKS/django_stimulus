6ac0966dbf63f35a25b6497a4dc165d9cf90b8cb
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-09 22:38:38 +0900

    fix

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def index(request)
-    return HttpResponse('<div controller=sample><div data-target="sample.hello"><div><script src="../assets/index.js"></script>')
+def index(request):
+    return HttpResponse('<div controller=sample><div data-target="sample.hello"></div></div><script src="../assets/index.js"></script>')
