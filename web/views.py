a17c5bd08b623171454f56395705d88b8a3c2ed8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-09 22:33:06 +0900

    add response

--- web/views.py
+++ web/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request)
+    return HttpResponse('<div controller=sample><div data-target="sample.hello"><div><script src="../assets/index.js"></script>')
